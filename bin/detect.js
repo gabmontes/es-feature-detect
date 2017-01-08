d752fc3aed5bce1c0a1edb0196df6cfb875e7837
Gabriel Montes <gabriel@utoctadel.com.ar>
2017-01-08 15:01:12 -0300

    Sorts output of node-feature-detect

--- bin/detect.js
+++ bin/detect.js
@@ -11,7 +11,7 @@ var features = require('../')
 console.log('Node version: ' + process.version)
 console.log()
 console.log('Features:')
-Object.keys(features).forEach(function (name) {
+Object.keys(features).sort().forEach(function (name) {
   var prettyName = splitCC(name).join(' ').toLowerCase()
   var isSupported = features[name]
   var supported = isSupported ? 'supported'.green : 'not supported'.red
